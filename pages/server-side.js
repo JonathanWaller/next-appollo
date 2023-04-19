6817d2671e0db1a318ea83c214cca9e6d858c80c
JonathanWaller <waller.j27@gmail.com>
2023-04-19 14:22:35 -0400

    setup

---
+++ pages/server-side.js
@@ -0,0 +1,54 @@
+import styles from '@/styles/Home.module.css'
+
+import { gql } from "@apollo/client";
+import client from '../apollo-client';
+
+export async function getServerSideProps() {
+  const { data } = await client.query({
+    query: gql`
+      query Countries {
+        countries {
+          code
+          name
+          emoji
+        }
+      }
+    `,
+  });
+
+  return {
+    props: {
+      countries: data.countries.slice(0, 4),
+    },
+ };
+}
+
+
+export default function Home({countries}) {
+
+  console.log('ok now: ', countries)
+
+  return(
+    <div className={styles.grid}>
+      {countries.map((country) => (
+        <div key={country.code} className={styles.card}>
+          <h3>
+            {/* <a 
+              href="#country-name" 
+              aria-hidden="true" 
+              class="aal_anchor" 
+              id="country-name"
+            >
+            </a> */}
+            {country.name}
+          </h3>
+          <p>
+            {country.code} - {country.emoji}
+          </p>
+        </div>
+      ))}
+    </div>
+  )
+  
+  
+}
